b8c901c8af0449bff8080685016a2ec7d01907ed
asad.pstu <asad.zaman@apsissolutions.com>
2022-06-27 20:11:54 +0600

    minor change

--- controllers/loanController.js
+++ controllers/loanController.js
@@ -400,9 +400,10 @@ exports.disbursement = async (req, res) => {
           .where("cr_sales_agent_user.sales_agent_id", sales_agent_id)
           .select(
             "cr_users.device_token"
-          );
-        console.log("device_token", device_token)  
+          ); 
         let receiver_token = device_token_response[0].device_token;
+        console.log("device_token", device_token_response)
+
         await pushNotification(retailer_id, sales_agent_id, "DISBURSEMENT", "Disbursement completed!", body, receiver_token)
       }
       return res.send(
