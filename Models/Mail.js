e9909bce82ce70eae6b53f0e6b707bca3818357f
asad.pstu <asad.zaman@apsissolutions.com>
2022-07-06 13:01:36 +0600

    email configuration

--- Models/Mail.js
+++ Models/Mail.js
@@ -1,9 +1,19 @@
 const knex = require("../config/database");
 const nodemailer = require("nodemailer");
 
+const { EMAIL,EMAIL_PASS,EMAIL_SERVICE,EMAIL_PORT} = process.env;
+
+
+const transportOptions = {
+  host: EMAIL_SERVICE,
+  port: EMAIL_PORT,
+  auth: { user: EMAIL, pass: EMAIL_PASS },
+  secureConnection: true,
+  tls: { ciphers: 'SSLv3' }
+};
+
 exports.saveMail = async (req, res) => {
   const { type, mail_subject, mail_body } = req.body;
-
   await knex("APSISIPDC.cr_sendmail")
     .insert([{ type: type, mail_subject: mail_subject, mail_body: mail_body }])
     .then((data) => res.json(data))
@@ -24,18 +34,18 @@ exports.sendEmail = async (req, res) => {
     .select("type", "mail_subject", "mail_body");
   const { type, mail_subject, mail_body } = data[0];
 
-  const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  const transporter = nodemailer.createTransport(transportOptions);
+  const mailOptions = {
+    from: FROM,
+    to: "asad.zaman@apsissolutions.com",
+    subject: "Email Functionality Quick Test",
+    html: "This is a test message!\n",
+  };
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      informationLog.error(error);
+      console.log(error);
     } else {
-      informationLog.info({
+      console.log({
         message: "Email sent" + info.response,
         time: Date.now(),
       });
@@ -46,17 +56,11 @@ exports.sendEmail = async (req, res) => {
 
  
 exports.sendEmailTemp = async (req, res) => {
-  const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
- console.log('mailbody',req.body)
+const transporter = nodemailer.createTransport(transportOptions);
+console.log('mailbody',req.body)
 
 const mailOptions = {
-  from: process.env.EMAIL,
+  from: FROM,
   to: req.body.email,
   subject: req.body.mail_subject,
   html: req.body.mail_body,
