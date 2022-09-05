d5e95957bc254e802fc3ae61207bff38f0ab55d1
jobyerarfin <syed.jobyer@apsissolutions.com>
2022-09-05 16:42:47 +0600

    status code changed

--- controllers/salesAgent.js
+++ controllers/salesAgent.js
@@ -14,7 +14,7 @@ exports.uploadSalesAgentOnboardingFile = async (req, res) => {
     const upload = await importExcelData2DB(req.file.filename, req.body);
     res.status(200).send(upload);
   } else {
-    res.status(200).send("File is Missing!");
+    res.status(404).send("File not Found!");
   }
 
 };
